4f940c7c5827cc88f4699587c350039e1b9643c7
moeik <i@tecat.cn>
2019-10-19 16:52:54 +0800

    Update music.js

--- assets/js/music.js
+++ assets/js/music.js
@@ -1,3 +1,20 @@
 function music() {
-    document.getElementById("bgAudio").volume = 0.2;
+    var userAgentInfo = navigator.userAgent;
+    var Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+    var flag = true;
+    for (var v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+    }
+    if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
+        document.getElementById("bgAudio").volume = 0.5;
+    } else if (/(Android)/i.test(navigator.userAgent)) {
+        document.getElementById("bgAudio").volume = 0.5;
+    } else {
+        document.getElementById("bgAudio").volume = 0.1;
 }
+
